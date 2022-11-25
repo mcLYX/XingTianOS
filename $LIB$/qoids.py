6c29272cbdb0c7e18c11e18b3e8a39d680f0405b
mcLYX <109810165+mcLYX@users.noreply.github.com>
2022-11-25 22:56:23 +0800

    Fix init color.
    🐎的单片机被收了不知道能不能跑

--- $LIB$/qoids.py
+++ $LIB$/qoids.py
@@ -8,7 +8,6 @@ scr.f(0);scr.s()
 bytemin=8#加载的最少字节数(8,不要改,除非QOI规范变了)
 loadstep=96#每次加载多少字节，更改可能影响速度
 cachesize=64#解码后的缓存大小，更改可能影响速度
-#cache=b''#24/32位色缓存
 cache565=b''#16位色缓存
 dat=bytearray(2)
 
@@ -43,14 +42,17 @@ else:
   #获取图像信息
   w=f.read(4)
   width=(w[0]<<24)+(w[1]<<16)+(w[2]<<8)+w[3]
-  del w
   h=f.read(4)
   height=(h[0]<<24)+(h[1]<<16)+(h[2]<<8)+h[3]
-  del h
-  tft.setarea([0,0],[width-1,height-1])
   channels=f.read(1)[0]
   colorspace=f.read(1)[0]#这玩意干啥的？？
   print(width,height,channels)
+  scr.t('Width:'+str(width),0,9)
+  scr.t('Height:'+str(height),0,18)
+  scr.t('Depth:'+str(channels*8),0,27)
+  scr.t('Size:'+("%.2f"%(os.stat(FileNAME)[6]/1024))+'KB',0,54)
+  scr.s()
+  tft.setarea([0,0],[width-1,height-1])
   channels=3#强制RGB
 
   #创建index
@@ -58,87 +60,48 @@ else:
   
   #开始图像数据解码
   byte=bytearray(f.read(loadstep))
-  rgba=b''
+  rgba=bytearray((0,0,0,255))
   
   while byte:
     
     byte+=bytearray(f.read(loadstep))
     
     while len(byte)>bytemin:
-      if byte[0]==0b11111110:#RGB
-        rgba=byte[1:4]+(rgba[-1:] if len(rgba)==4 else b'\xff')
-        #cache+=rgba[:channels]
-        byte=byte[4:]
-        #input('Detected RGB')
-        
-      elif byte[0]==0b11111111:#RGBA
-        rgba=byte[1:5]
-        #cache+=rgba[:channels]
-        byte=byte[5:]
-        #input('Detected RGBA')
+      if byte[0]>=0b11111110:#RGB(A)
+        rgba=byte[1:byte[0]-250]+(rgba[-1:] if (byte[0]-255) else b'')
+        byte=byte[byte[0]-250:]
         
       elif (byte[0]>>6)==0:#INDEX
         indexp=byte[0]&0b00111111
         rgba=index[indexp*4:(1+indexp)*4]
-        #cache+=rgba[:channels]
-        #del byte[0]
         byte=byte[1:]
-        #input('Detected INDEX')
         
       elif (byte[0]>>6)==1:#DIFF
-        #indexp=len(cache)-channels-1
-        prev=rgba
-        rgba=bytes([(prev[-4] + (byte[0]>>4)%4-2) %256])
-        rgba+=bytes([(prev[-3] + (byte[0]>>2)%4-2) %256])
-        rgba+=bytes([(prev[-2] + byte[0]%4-2) %256])
-        rgba+=bytes([prev[-1]])
-        #cache+=rgba[:channels]
-        #del byte[0]
+        rgba=bytearray(((rgba[-4] + (byte[0]>>4)%4-2) %256,(rgba[-3] + (byte[0]>>2)%4-2) %256,(rgba[-2] + byte[0]%4-2) %256,rgba[-1]))
         byte=byte[1:]
-        #input('Detected DIFF')
         
       elif (byte[0]>>6)==2:#LUMA
-        #indexp=len(cache)-channels-1
-        prev=rgba
         dg=byte[0]%64-32
         dr,db=(byte[1]>>4)+dg-8,(byte[1]%16)+dg-8
-        rgba=bytes([(prev[-4] + dr) %256])
-        rgba+=bytes([(prev[-3] + dg) %256])
-        rgba+=bytes([(prev[-2] + db) %256])
-        rgba+=bytes([prev[-1]])
-        #cache+=rgba[:channels]
-        #del byte[0:2]
+        rgba=bytearray(((rgba[-4] + dr) %256,(rgba[-3] + dg) %256,(rgba[-2] + db) %256,rgba[-1]))
         byte=byte[2:]
-        #input('Detected LUMA')
         
       elif (byte[0]>>6)==3:#RUN
-        prev=rgba
-        for i in range(byte[0]%64+1):
-          rgba=prev[-4:] if prev else b'\x00'*4
-          #cache+=rgba[:channels]
-          if i:rgb565(rgba)
-
+        for i in range(byte[0]%64):
+          rgb565(rgba)
         byte=byte[1:]
-        #input('Detected RUN')
         
       #写入INDEX
       if rgba:
         indexp=(rgba[-4]*3+rgba[-3]*5+rgba[-2]*7+rgba[-1]*11)%64
         index[indexp*4:indexp*4+4]=rgba
         rgb565(rgba)
-        
-      #满缓存
-      '''
-      if len(cache)>=cachesize:
-        cache=cache[loadstep:]
-      '''
-      
+
       #解码完毕
       if len(byte)==8 and byte==b'\x00'*7+b'\x01':
         rgb565()
         f.close()
         byte=b''
-        #del cache
         print('OK')
 
 print(time.ticks_ms()-t)
