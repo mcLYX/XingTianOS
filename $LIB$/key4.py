892e60341eacdec98f2489cac5ad28e2288b974d
mcLYX <109810165+mcLYX@users.noreply.github.com>
2023-04-13 22:26:35 +0800

    Update key4.py
    精简

--- $LIB$/key4.py
+++ $LIB$/key4.py
@@ -2,14 +2,9 @@ import machine
 vcc=machine.Pin(2,machine.Pin.OUT)
 vcc.on()
 
-keys=[machine.Pin(6,machine.Pin.IN,machine.Pin.PULL_DOWN),
-      machine.Pin(4,machine.Pin.IN,machine.Pin.PULL_DOWN),
-      machine.Pin(10,machine.Pin.IN,machine.Pin.PULL_DOWN),
-      machine.Pin(8,machine.Pin.IN,machine.Pin.PULL_DOWN)]
+keys=[machine.Pin(i,machine.Pin.IN,machine.Pin.PULL_DOWN) for i in (6,4,10,8)]
 
 def get(key=None):
-    value=[]
-    for k in keys:
-        value.append(k.value())
+    value=[k.value() for k in keys]
     if key:return value[key-1]
     return value
